703cb0d431c2a03c67658a120de5e34b5e1fa98e
Kevin Grant <kevingrant5@gmail.com>
2024-08-05 13:58:48 -0400

    Add AI stuff

--- src/MyPlayer.cpp
+++ src/MyPlayer.cpp
@@ -7,6 +7,9 @@
 #include "Config.h"
 #include "Chat.h"
 
+// Define the LLaMATokenizer type
+typedef std::unique_ptr<LLaMATokenizer> LLaMATokenizerPtr;
+
 enum MyPlayerAcoreString
 {
     HELLO_WORLD = 35410
@@ -22,10 +25,19 @@ public:
     {
         ChatHandler(player->GetSession()).PSendSysMessage("OnLogin called");
 
+        // Initialize the LLaMATokenizer model
+        std::string modelPath = "/root/llama-models/models/llama3_1/Meta-Llama-3.1-8B/consolidated.00.pth";
+        LLaMATokenizerPtr tokenizer(new LLaMATokenizer(modelPath));
 
+        // Use the tokenizer to process user input
         if (sConfigMgr->GetOption<bool>("MyModule.Enable", false))
         {
             ChatHandler(player->GetSession()).PSendSysMessage("Hello, world!");
+
+            // Process user input using the LLaMATokenizer
+            std::string userInput = "What's your name?";
+            auto response = tokenizer->processInput(userInput);
+            ChatHandler(player->GetSession()).PSendSysMessage(response.data());
         }
         else
         {
